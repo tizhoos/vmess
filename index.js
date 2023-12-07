604282e98cbc7de91a07763a318a4dd1ec825c35
tizhoos <152868687+tizhoos@users.noreply.github.com>
2023-12-07 21:40:36 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,44 @@
-// telegramChannelReader.js
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-module.exports = {
-    getRandomVmessURIs: async function(channels) {
-        const channel = channels[Math.floor(Math.random() * channels.length)];
-        try {
-            const response = await axios.get('https://t.me/s/' + channel);
-            const $ = cheerio.load(response.data);
-            const dataList = [];
-            const regex = /vmess:\/\/([a-zA-Z0-9+\/=]+)/gi;
-            $('body').text().match(regex).slice(0, 5).forEach((match) => {
-                dataList.push(match);
-            });
-            return {
-                status: 200,
-                result: dataList
-            };
-        } catch (error) {
-            throw new Error(error);
-        }
-    }
+const channels = [
+  "VpnWLF",
+  "Awlix_ir",
+  "FreakConfig",
+  "yaney_01",
+  "DeamNet_Proxy",
+  "forwardv2ray",
+  "free4allVPN",
+  "oneclickvpnkeys",
+  "Hope_Net",
+  "v2Line",
+  "ConfigsHUB",
+  "v2ray1_ng",
+  "v2ray_outlineir",
+  "ShadowProxy66",
+  "proxystore11",
+  "V2rayCollectorDonate",
+  "prrofile_purple",
+  "mftizi",
+  "iP_CF"
+];
+
+const channel = channels[Math.floor(Math.random() * channels.length)];
+
+const response = await axios.get(`https://t.me/s/${channel}`);
+const $ = cheerio.load(response.data);
+
+const vmessUris = [];
+
+$("a[href^='vmess://']").each((i, el) => {
+  vmessUris.push(el.attribs.href);
+});
+
+const res = vmessUris.slice(0, 5);
+
+const json = {
+  status: 200,
+  result: res
 };
+
+console.log(JSON.stringify(json, null, 2));
